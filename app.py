3c7789da9f1fba5996af6f3e0516ccfae2b31874
radadiavasu <114792837+radadiavasu@users.noreply.github.com>
2023-08-03 20:15:24 +0530

    app.py

--- app.py
+++ app.py
@@ -40,15 +40,17 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
+    if input_sms.strip() == "":
+        st.header("Please provide a valid message!!!")
     else:
-        st.header("Not Spam")
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
